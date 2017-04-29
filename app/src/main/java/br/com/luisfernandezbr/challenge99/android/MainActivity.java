158b3637330e92f44f300400b52e6ef41ce6f6fa
Luis Fernandez <luis82fernandez@gmail.com>
2017-04-29 01:11:01 -0300

    Organizing project structure. Moving MainActivity to new package.

--- app/src/main/java/br/com/luisfernandezbr/challenge99/android/MainActivity.java
+++ app/src/main/java/br/com/luisfernandezbr/challenge99/android/MainActivity.java
@@ -1,7 +1,9 @@
-package br.com.luisfernandezbr.challenge99;
+package br.com.luisfernandezbr.challenge99.android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import br.com.luisfernandezbr.challenge99.R;
 
 public class MainActivity extends AppCompatActivity {
 
