a161124769f1946d8ddd4d11adffe351c0548023
Luis Fernandez <luis82fernandez@gmail.com>
2017-04-29 16:27:28 -0300

    Adding List<TechStars> param on DataAccess success method

--- app/src/main/java/br/com/luisfernandezbr/challenge99/mvp/dataaccess/TechStarsDataAccess.java
+++ app/src/main/java/br/com/luisfernandezbr/challenge99/mvp/dataaccess/TechStarsDataAccess.java
@@ -1,8 +1,12 @@
 package br.com.luisfernandezbr.challenge99.mvp.dataaccess;
 
 
+import java.util.List;
+
+import br.com.luisfernandezbr.challenge99.pojo.TechStar;
+
 public interface TechStarsDataAccess {
 
     void loadList();
-    void sendDataAccessListSuccessEvent();
+    void sendDataAccessListSuccessEvent(List<TechStar> techStarsList);
 }
