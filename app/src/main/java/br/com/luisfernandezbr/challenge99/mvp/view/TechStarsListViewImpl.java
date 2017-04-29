24abc1565770d5a12b86c0bf558d435106f2934f
Luis Fernandez <luis82fernandez@gmail.com>
2017-04-29 18:45:13 -0300

    Creating View implementation

---
+++ app/src/main/java/br/com/luisfernandezbr/challenge99/mvp/view/TechStarsListViewImpl.java
@@ -0,0 +1,29 @@
+package br.com.luisfernandezbr.challenge99.mvp.view;
+
+
+import java.util.List;
+
+import br.com.luisfernandezbr.challenge99.pojo.TechStar;
+
+class TechStarsListViewImpl implements TechStarsListView {
+
+    @Override
+    public void sendViewLoadListEvent() {
+
+    }
+
+    @Override
+    public void sendViewItemClickedEvent() {
+
+    }
+
+    @Override
+    public void showLoadListSuccess(List<TechStar> techStarsList) {
+
+    }
+
+    @Override
+    public void showLoadListError(int errorCode, String errorMessage) {
+
+    }
+}
