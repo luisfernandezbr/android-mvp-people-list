0d610c7e71fa5fb8ebc581866caf7cfbd88a5667
Luis Fernandez <luis82fernandez@gmail.com>
2017-04-29 17:26:51 -0300

    Implementing load from raw resource method

--- app/src/main/java/br/com/luisfernandezbr/challenge99/android/AndroidUtils.java
+++ app/src/main/java/br/com/luisfernandezbr/challenge99/android/AndroidUtils.java
@@ -5,10 +5,32 @@ import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.RawRes;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringWriter;
+import java.io.Writer;
+
 public class AndroidUtils {
 
-    public static String getJsonFromRaw(@NonNull Context context, @RawRes int resRawId) {
-        return null;
+    public static String getJsonFromRaw(@NonNull Context context, @RawRes int resRawId) throws IOException {
+        InputStream inputStream = context.getResources().openRawResource(resRawId);
+        Writer writer = new StringWriter();
+        char[] buffer = new char[1024];
+
+        try {
+            Reader reader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
+            int n;
+            while ((n = reader.read(buffer)) != -1) {
+                writer.write(buffer, 0, n);
+            }
+        } finally {
+            inputStream.close();
+        }
+
+        return writer.toString();
     }
 
 }
