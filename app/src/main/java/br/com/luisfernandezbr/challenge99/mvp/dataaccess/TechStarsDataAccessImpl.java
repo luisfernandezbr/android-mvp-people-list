dd972ce3704164ddc0b2e896ac457c82ca1e7798
Luis Fernandez <luis82fernandez@gmail.com>
2017-04-29 17:17:03 -0300

    Implementing success flow on DataAccess.loadList()

--- app/src/main/java/br/com/luisfernandezbr/challenge99/mvp/dataaccess/TechStarsDataAccessImpl.java
+++ app/src/main/java/br/com/luisfernandezbr/challenge99/mvp/dataaccess/TechStarsDataAccessImpl.java
@@ -1,15 +1,27 @@
 package br.com.luisfernandezbr.challenge99.mvp.dataaccess;
 
 
+import android.content.Context;
+
 import java.util.List;
 
+import br.com.luisfernandezbr.challenge99.R;
+import br.com.luisfernandezbr.challenge99.android.AndroidUtils;
 import br.com.luisfernandezbr.challenge99.pojo.TechStar;
 
 public class TechStarsDataAccessImpl implements TechStarsDataAccess {
 
+    private Context context;
+
     @Override
     public void loadList() {
+        String jsonFromRaw = AndroidUtils.getJsonFromRaw(context, R.raw.mock_data_list);
 
+        List<TechStar> techStarsList = this.getFromJson(jsonFromRaw);
+
+        if (this.isValidList(techStarsList)) {
+            this.sendDataAccessListSuccessEvent(techStarsList);
+        }
     }
 
     @Override
