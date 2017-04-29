5009d66acf5ad274447c3b0784d119cf7ed627a6
Luis Fernandez <luis82fernandez@gmail.com>
2017-04-29 16:25:09 -0300

    Adding Android util class to load json from /res/raw folder

---
+++ app/src/main/java/br/com/luisfernandezbr/challenge99/android/AndroidUtils.java
@@ -0,0 +1,14 @@
+package br.com.luisfernandezbr.challenge99.android;
+
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.RawRes;
+
+public class AndroidUtils {
+
+    public static String getJsonFromRaw(@NonNull Context context, @RawRes int resRawId) {
+        return null;
+    }
+
+}
