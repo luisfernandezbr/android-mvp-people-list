e645afbc6c82879613f4e068d076af3c6e10f1b4
Luis Fernandez <luis82fernandez@gmail.com>
2017-04-29 01:20:37 -0300

    Creating TechStar pojo

---
+++ app/src/main/java/br/com/luisfernandezbr/challenge99/pojo/TechStar.java
@@ -0,0 +1,53 @@
+package br.com.luisfernandezbr.challenge99.pojo;
+
+
+import java.io.Serializable;
+
+public class TechStar implements Serializable {
+
+    private String id;
+    private String name;
+    private String image;
+    private String birthday;
+    private String bio;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+}
