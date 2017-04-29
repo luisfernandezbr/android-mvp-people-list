108811b6639f5238177ae32e2ad033baea160539
Luis Fernandez <luis82fernandez@gmail.com>
2017-04-29 18:47:34 -0300

    Extending BaseView from mvp library

--- app/src/main/java/br/com/luisfernandezbr/challenge99/mvp/view/TechStarsListViewImpl.java
+++ app/src/main/java/br/com/luisfernandezbr/challenge99/mvp/view/TechStarsListViewImpl.java
@@ -3,9 +3,26 @@ package br.com.luisfernandezbr.challenge99.mvp.view;
 
 import java.util.List;
 
+import br.com.luisfernandezbr.challenge99.R;
 import br.com.luisfernandezbr.challenge99.pojo.TechStar;
+import br.com.luisfernandezbr.mvp.ActivityLoader;
+import br.com.luisfernandezbr.mvp.BaseView;
 
-class TechStarsListViewImpl implements TechStarsListView {
+class TechStarsListViewImpl extends BaseView implements TechStarsListView {
+
+    public TechStarsListViewImpl(ActivityLoader activityLoader) {
+        super(activityLoader);
+    }
+
+    @Override
+    protected int getLayoutResId() {
+        return R.layout.activity_techstars_list;
+    }
+
+    @Override
+    protected void initViews() {
+
+    }
 
     @Override
     public void sendViewLoadListEvent() {
