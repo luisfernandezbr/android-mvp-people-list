a3d5d4315399285544b84bc0d3b54e4a089e9ff4
Luis Fernandez <luis82fernandez@gmail.com>
2017-04-29 18:09:57 -0300

    Using new Service Layer

--- app/src/main/java/br/com/luisfernandezbr/challenge99/mvp/dataaccess/TechStarsDataAccess.java
+++ app/src/main/java/br/com/luisfernandezbr/challenge99/mvp/dataaccess/TechStarsDataAccess.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 import br.com.luisfernandezbr.challenge99.pojo.TechStar;
 
-public interface TechStarsDataAccess {
+interface TechStarsDataAccess {
 
     void loadList();
     void sendDataAccessListSuccessEvent(List<TechStar> techStarsList);
--- app/src/main/java/br/com/luisfernandezbr/challenge99/mvp/dataaccess/TechStarsDataAccessImpl.java
+++ app/src/main/java/br/com/luisfernandezbr/challenge99/mvp/dataaccess/TechStarsDataAccessImpl.java
@@ -1,34 +1,23 @@
 package br.com.luisfernandezbr.challenge99.mvp.dataaccess;
 
 
-import android.content.Context;
 import android.util.Log;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-
 import java.io.IOException;
-import java.lang.reflect.Type;
 import java.util.List;
 
-import br.com.luisfernandezbr.challenge99.R;
-import br.com.luisfernandezbr.challenge99.android.AndroidUtils;
 import br.com.luisfernandezbr.challenge99.pojo.TechStar;
 
 public class TechStarsDataAccessImpl implements TechStarsDataAccess {
 
-    public static final String TAG = "TechStarsDataAccessImpl";
+    private static final String TAG = "TechStarsDataAccessImpl";
 
-    private Context context;
+    private DataService rawDataService;
 
     @Override
     public void loadList() {
-        String jsonFromRaw = null;
-
         try {
-            jsonFromRaw = AndroidUtils.getJsonFromRaw(context, R.raw.mock_data_list);
-
-            List<TechStar> techStarsList = this.getFromJson(jsonFromRaw);
+            List<TechStar> techStarsList = this.rawDataService.loadList();
 
             if (this.isValidList(techStarsList)) {
                 this.sendDataAccessListSuccessEvent(techStarsList);
@@ -56,14 +45,5 @@ public class TechStarsDataAccessImpl implements TechStarsDataAccess {
 
     private boolean isValidList(List<TechStar> techStarsList) {
         return techStarsList != null && techStarsList.size() > 0;
-    }
-
-    private List<TechStar> getFromJson(String json) {
-        Gson gson = new Gson();
-
-        Type type = new TypeToken<List<TechStar>>() {
-        }.getType();
-
-        return gson.fromJson(json, type);
     }
 }
